9d39ca7055707529cc8d3dc72dc5930c4b3f6e87
binuhp7 <binuhp7@gmail.com>
2025-10-31 19:43:29 +0530

    Update insta.sh

--- insta.sh
+++ insta.sh
@@ -66,8 +66,8 @@ function start() {
 banner
 checkroot
 dependencies
-read -p $'\e[1;92m 📝 Enter The Username For The Account: \e[0m' user
-checkaccount=$(curl -s https://www.instagram.com/$user/?__a=1 | grep -c "The User May Have Been Banned Or Page Being Deleted/Non-Exsistance/No User (With this username)")
+read -p $'\e[1;92m 📝 Enter The Username For The Account: \e[0m' cristal_me010
+checkaccount=$(curl -s https://www.instagram.com/$user/?__a=1 | grep -c "The User May Have Been Banned Or Page Being Deleted/Non-Exsistance/No User (cristal_me010)")
 if [[ "$checkaccount" == 1 ]]; then
 printf "\e[1;91m ❌ Invalid Username Entered! Try Again\e[0m\n"
 sleep 1
